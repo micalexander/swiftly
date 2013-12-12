e4676c82dba321d3384c9f3bdda5c3dec121a82e
micalexander <micalexander@hotmail.com>
2013-12-12 12:54:55 -0700

    edit to published date generator

--- lib/obi/post_type.rb
+++ lib/obi/post_type.rb
@@ -20,27 +20,23 @@ module Obi
 		end
 
 		def create_post
-
+			@match_2 = ''
 			if options[:filter_by] == 'published-date' or options[:filter_by] == ''
-				@filter_by = :published_date
+				@filter_by = '&int-year=$matches[1]'
+				@filter_by_regexp = '(\d{4})/(\d{2})/??$'
+				@match_2 = '&int-month=$matches[2]'
 			elsif options[:filter_by] == 'last-name'
-				@filter_by = :letter
+				@filter_by = '&letter=$matches[1]'
 				@filter_by_regexp = '([A-Z])/??$'
-				rewrite = true
 			elsif options[:filter_by] == 'start-date'
-				@filter_by = :month
+				@filter_by = '&month=$matches[1]'
 				@filter_by_regexp = '(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)/?$'
-				rewrite = true
 			else
 				say "\nobi: filter type \"#{options[:filter_by]}\" unaccepted\n", :red
 				exit
 			end
 
-			if rewrite
-				template File.join( 'templates', 'post_type_rewrite.erb' ), File.join( "#{project_path}", 'wp-content', 'plugins', "#{project_name}-specific-plugin", "#{post_type}","#{post_type}.php")
-			else
-				template File.join( 'templates', 'post_type.erb' ), File.join( "#{project_path}", 'wp-content', 'plugins', "#{project_name}-specific-plugin", 'custom-post-types',"#{post_type}.php")
-			end
+			template File.join( 'templates', 'post_type_rewrite.erb' ), File.join( "#{project_path}", 'wp-content', 'plugins', "#{project_name}-specific-plugin", 'custom-post-types',"#{post_type}.php")
 		end
 	end
 end
