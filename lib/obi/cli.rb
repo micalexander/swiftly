60cdcc88ab429c01c825d73250cdbd36d2e73f7f
micalexander <micalexander@hotmail.com>
2015-07-10 21:15:55 -0700

    cleaned up cli and environment rb and restuctured evriroment rb to read json for wp-config params

--- lib/obi/cli.rb
+++ lib/obi/cli.rb
@@ -1,4 +1,5 @@
 require 'Thor'
+require 'json'
 require 'obi/Obi_module'
 require 'obi/templates_config'
 require 'obi/plugins_config'
@@ -12,6 +13,7 @@ require 'obi/Rsync'
 
 module Obi
 	class CLI < Thor
+
 		include Thor::Actions
 		include Obi::GetCurrentDirectoryBasename
 		include Obi::ProjectExist
@@ -19,17 +21,20 @@ module Obi
 
 		# Handles the creation of the .obiconfig file
 		desc "config", "Configure obi. (mandatory for first time and future use)"
+
 		def config
+
 			Upgrade.check
 			Configuration.check
 			Menu.new.launch!
+
 		end
 
 		desc "upgrade", "Upgrade obi to the current version"
 
 		method_option :project, :aliases => "-p", :type => :boolean, :desc => "Upgrade project config"
-		method_option :global, :aliases => "-g", :type => :boolean, :desc => "Upgrade global config"
-		method_option :all, :aliases => "-a", :type => :boolean, :desc => "Upgrade both global and project configs"
+		method_option :global , :aliases => "-g", :type => :boolean, :desc => "Upgrade global config"
+		method_option :all    , :aliases => "-a", :type => :boolean, :desc => "Upgrade both global and project configs"
 
 		def upgrade( project_name = nil )
 
@@ -48,64 +53,91 @@ module Obi
 				Upgrade.global_config
 
 			else
+
 				say
 				say "obi: Not sure what you are trying to do";
 				say
 				say `obi -h upgrade`
 				say
+
 			end
 		end
 
 		desc "plugins", "Create global plugin directory in working project directory"
+
 		def plugins
+
 			Upgrade.check
-            Configuration.settings
-           	PluginsConfig.new.create
+      Configuration.settings
+     	PluginsConfig.new.create
+
 		end
 
 		desc "templates", "Create global template directory in working project directory"
+
 		def templates
+
 			Upgrade.check
-            Configuration.settings
-           	TemplatesConfig.new.create
+			Configuration.settings
+			TemplatesConfig.new.create
+
 		end
 
 
 		desc "new [option] [project_name]", "Create a new projects by passing a project name"
 
-		method_option :empty, :aliases => "-e", :type => :boolean, :desc => "Create an empty project"
-		method_option :git, :aliases => "-g", :type => :boolean, :desc => "Create a Git enabled project"
+		method_option :empty    , :aliases => "-e", :type => :boolean, :desc => "Create an empty project"
+		method_option :git      , :aliases => "-g", :type => :boolean, :desc => "Create a Git enabled project"
 		method_option :wordpress, :aliases => "-w", :type => :boolean, :desc => "Create a project with Wordpress installed"
-		method_option :dev, :aliases => "-d", :type => :boolean, :desc => "If Wordpress option is used run bower and guard"
+		method_option :dev      , :aliases => "-d", :type => :boolean, :desc => "If Wordpress option is used run bower and guard"
 
 		def new(project_name, template = '')
 
 			Upgrade.check
+
 			project_name = get_current_directory_basename(project_name)
+
       Obi::Configuration.settings
+
 			project = Obi::Project.new(project_name)
 
 			if options.length == 1
+
 				if options[:empty]
+
 					project.empty
+
 				elsif options[:git]
+
 					project.git
+
 				elsif options[:wordpress]
+
 					project.wordpress template
+
 				end
+
 			elsif options.length == 2
+
 				if options[:wordpress] && options[:dev]
+
 					project.wordpress template, options[:dev]
+
 				end
+
 			else
-				puts
-				puts "obi: new expects at least one option"
-				puts
+
+				say
+				say "obi: new expects at least one option"
+				say
+
 			end
 		end
 
 		no_tasks do
+
 			alias_method :n, :new
+
 		end
 
 		desc "destroy [option] [project_name]", "Remove a projects by passing a project name"
@@ -113,77 +145,97 @@ module Obi
 		def kill(project_name)
 
 			Upgrade.check
+
 			project_name = get_current_directory_basename(project_name)
-			database = Database.new(project_name)
-			credentials = Environment.new
+			database     = Database.new(project_name)
+			credentials  = Environment.new
 
 			database.dump( credentials.environment_settings( project_name, "local" ) )
 			database.drop( credentials.environment_settings( project_name, "local" ) )
 
-		 	directory = File.join( Configuration.settings['local_project_directory'], project_name + '/' )
-		 	zipfile_name = File.join( Configuration.settings['local_project_directory'], project_name + '.zip' )
+			directory    = File.join( Configuration.settings['local_project_directory'], project_name + '/' )
+			zipfile_name = File.join( Configuration.settings['local_project_directory'], project_name + '.zip' )
 
 		 	if File.exist? zipfile_name
 
-			 	puts ""
-			 	puts "obi: Can't zip directory. There is already a zip file named [ #{project_name}.zip ]"
-			 	puts ""
+			 	say ""
+			 	say "obi: Can't zip directory. There is already a zip file named [ #{project_name}.zip ]"
+			 	say ""
 			 	exit
 
 		 	else
 
 				Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
-				    Dir[File.join(directory, '**', '**')].each do |file|
-				      zipfile.add(file.sub(directory, ''), file)
-				    end
+
+			    Dir[File.join(directory, '**', '**')].each do |file|
+
+			      zipfile.add(file.sub(directory, ''), file)
+
+			    end
 				end
+
 				FileUtils.remove_dir( File.join( Configuration.settings['local_project_directory'], project_name ))
+
 			end
 		end
 
 		no_tasks do
+
 			alias_method :k, :kill
+
 		end
 
 		desc "database [option] [project_name]", "Maintain your databases by passing a project name"
 
-		method_option :backup, :aliases => "-b", :type => :boolean, :desc => "Backup database"
-		method_option :import, :aliases => "-i", :type => :boolean, :desc => "Import file into database"
-		method_option :sync, :aliases => "-y", :type => :boolean, :desc => "Sync two databases"
-		method_option :local, :aliases => "-l", :type => :boolean, :desc => "Local"
-		method_option :staging, :aliases => "-s", :type => :boolean, :desc => "Staging"
-		method_option :production, :aliases => "-p", :type => :boolean, :desc => "Production"
-		method_option :lts, :type => :boolean, :desc => "Local to staging"
-		method_option :ltp, :type => :boolean, :desc => "Local to production"
-		method_option :stl, :type => :boolean, :desc => "Staging to local"
-		method_option :stp, :type => :boolean, :desc => "Staging to production"
-		method_option :ptl, :type => :boolean, :desc => "Production to local"
-		method_option :pts, :type => :boolean, :desc => "Production to staging"
+		method_option :backup    , :aliases => "-b"    , :type => :boolean, :desc => "Backup database"
+		method_option :import    , :aliases => "-i"    , :type => :boolean, :desc => "Import file into database"
+		method_option :sync      , :aliases => "-y"    , :type => :boolean, :desc => "Sync two databases"
+		method_option :local     , :aliases => "-l"    , :type => :boolean, :desc => "Local"
+		method_option :staging   , :aliases => "-s"    , :type => :boolean, :desc => "Staging"
+		method_option :production, :aliases => "-p"    , :type => :boolean, :desc => "Production"
+		method_option :lts       , :type    => :boolean, :desc => "Local to staging"
+		method_option :ltp       , :type    => :boolean, :desc => "Local to production"
+		method_option :stl       , :type    => :boolean, :desc => "Staging to local"
+		method_option :stp       , :type    => :boolean, :desc => "Staging to production"
+		method_option :ptl       , :type    => :boolean, :desc => "Production to local"
+		method_option :pts       , :type    => :boolean, :desc => "Production to staging"
 
 
 		def database( project_name, import_file = "" )
 
 			Upgrade.check
+
 			project_name = get_current_directory_basename(project_name)
-			database = Obi::Database.new( project_name )
+			database     = Obi::Database.new( project_name )
 
 			# check to see if one of the options was backup
 			if options.keys.count == 2 and options[:backup]
 
 				# dump the specified database
 				if options[:local]
+
 					origin_credentials = Obi::Environment.new.environment_settings(project_name, "local")
+
 					database.dump( origin_credentials )
+
 				elsif options[:staging]
+
 					origin_credentials = Obi::Environment.new.environment_settings(project_name, "staging")
+
 					database.dump( origin_credentials )
+
 				elsif options[:production]
+
 					origin_credentials = Obi::Environment.new.environment_settings(project_name, "production")
+
 					database.dump( origin_credentials )
+
 				else
+
 					say
 					say "obi: The --#{options.keys[0]} option can not be passed by itself"
 					say
+
 				end
 
 			# check to see if one of the options was import and a file was provided
@@ -191,14 +243,20 @@ module Obi
 
 				# get the necessary database credentials
 				if options[:local]
-					origin_credentials = Obi::Environment.new.environment_settings(project_name, "local")
+
+					origin_credentials      = Obi::Environment.new.environment_settings(project_name, "local")
 					destination_credentials = Obi::Environment.new.environment_settings(project_name, "local")
+
 				elsif options[:staging]
-					origin_credentials = Obi::Environment.new.environment_settings(project_name, "staging")
+
+					origin_credentials      = Obi::Environment.new.environment_settings(project_name, "staging")
 					destination_credentials = Obi::Environment.new.environment_settings(project_name, "staging")
+
 				elsif options[:production]
-					origin_credentials = Obi::Environment.new.environment_settings(project_name, "production")
+
+					origin_credentials      = Obi::Environment.new.environment_settings(project_name, "production")
 					destination_credentials = Obi::Environment.new.environment_settings(project_name, "production")
+
 				end
 
 				# dump the destination database first!
@@ -212,41 +270,63 @@ module Obi
 
 				# get the necessary database credentials
 				if options[:lts]
-					origin_credentials = Obi::Environment.new.environment_settings(project_name, "local")
+
+					origin_credentials      = Obi::Environment.new.environment_settings(project_name, "local")
 					destination_credentials = Obi::Environment.new.environment_settings(project_name, "staging")
+
 				elsif options[:ltp]
-					origin_credentials = Obi::Environment.new.environment_settings(project_name, "local")
+
+					origin_credentials      = Obi::Environment.new.environment_settings(project_name, "local")
 					destination_credentials = Obi::Environment.new.environment_settings(project_name, "production")
+
 				elsif options[:stl]
-					origin_credentials = Obi::Environment.new.environment_settings(project_name, "staging")
+
+					origin_credentials      = Obi::Environment.new.environment_settings(project_name, "staging")
 					destination_credentials = Obi::Environment.new.environment_settings(project_name, "local")
+
 				elsif options[:stp]
-					origin_credentials = Obi::Environment.new.environment_settings(project_name, "staging")
+
+					origin_credentials      = Obi::Environment.new.environment_settings(project_name, "staging")
 					destination_credentials = Obi::Environment.new.environment_settings(project_name, "production")
+
 				elsif options[:ptl]
-					origin_credentials = Obi::Environment.new.environment_settings(project_name, "production")
+
+					origin_credentials      = Obi::Environment.new.environment_settings(project_name, "production")
 					destination_credentials = Obi::Environment.new.environment_settings(project_name, "local")
+
 				elsif options[:pts]
-					origin_credentials = Obi::Environment.new.environment_settings(project_name, "production")
+
+					origin_credentials      = Obi::Environment.new.environment_settings(project_name, "production")
 					destination_credentials = Obi::Environment.new.environment_settings(project_name, "staging")
+
 				end
 
 				# check to see if a file was provided if so find and replace urls and import it
 				if import_file.empty?
+
 					database.sync( origin_credentials, destination_credentials )
+
 				else
+
 					database.sync( origin_credentials, destination_credentials, import_file )
+
 				end
+
 			else
+
 				say
 				say "obi: Not sure what you are trying to do";
 				say
 				say `obi -h database`
 				say
+
 			end
 		end
+
 		no_tasks do
+
 			alias_method :d, :database
+
 		end
 
 		desc "rsync [option] [project_name]", "Mirror two directories by passing a project name, origin and destination"
@@ -261,70 +341,96 @@ module Obi
 		def rsync(project_name)
 
 			Upgrade.check
+
 			project_name = get_current_directory_basename(project_name)
-            Obi::Configuration.settings
+
+      Obi::Configuration.settings
+
 			if options.keys.count == 1
 
 				# get the necessary database credentials
 				if options[:lts]
-					origin = "local"
+
+					origin      = "local"
 					destination = "staging"
+
 				elsif options[:ltp]
-					origin = "local"
+
+					origin      = "local"
 					destination = "production"
+
 				elsif options[:stl]
-					origin = "staging"
+
+					origin      = "staging"
 					destination = "local"
+
 				elsif options[:stp]
-					origin = "staging"
+
+					origin      = "staging"
 					destination = "production"
+
 				elsif options[:ptl]
-					origin = "production"
+
+					origin      = "production"
 					destination = "local"
+
 				elsif options[:pts]
-					origin = "production"
+
+					origin      = "production"
 					destination = "staging"
+
 				end
 
 				Rsync.sync project_name, origin, destination
 
 			else
+
 				say
 				say "obi: Not sure what you are trying to do";
 				say
 				say `obi -h rsync`
 				say
+
 			end
 		end
 
 		no_tasks do
+
 			alias_method :r, :rsync
+
 		end
 
 		desc "generate [option] [project_name] [post_type]", "Generate wordpress templates"
 
 		method_option :custom_post_type, :aliases => "-c", :type => :boolean, :desc => "Generate custom post type"
 
-		def generate( project_name, post_type, filter_by = '' )#, post_type, filter_by )
+		def generate( project_name, post_type, filter_by = '' )
 
 			Upgrade.check
+
 			project_name = get_current_directory_basename(project_name)
-            Obi::Configuration.settings
+
+      Obi::Configuration.settings
+
 			if options.keys.count == 1
 
 				PostType.new([project_name, post_type, File.join( Configuration.settings['local_project_directory'], project_name)], {'filter_by' => "#{filter_by[/(.+[=])(.+)/, 2]}"}).invoke_all
+
 			else
+
 				say
 				say "obi: Not sure what you are trying to do";
 				say
 				say `obi -h generate`
 				say
+
 			end
 		end
 
 		no_tasks do
-			alias_method :g, :generate
-		end
 
+			alias_method :g, :generate
+
+		end
 	end
 end
--- lib/obi/environment.rb
+++ lib/obi/environment.rb
@@ -2,52 +2,48 @@ module Obi
 	class Environment
 
 		def initialize
+
 			@config_settings = Obi::Configuration.settings
 		end
 
 		def environment_settings(project_name, environment)
-			@environment = environment
-			@project_name = project_name
+
+			@environment         = environment
+			@project_name        = project_name
 			@environment_setting = @config_settings["#{@environment}_settings"]
+
 			case @environment_setting
+
 			when "enabled"
+
 				# load the obiconfig file environment settings for enabled environments
 				environment_settings  = {
-										host: "#{@config_settings["#{@environment}_host"]}",
-										user: "#{@config_settings["#{@environment}_user"]}",
-										pass: "#{@config_settings["#{@environment}_password"]}",
-										environment: "#{@environment}",
-										status: "\033[32menabled\033[0m"
-										}
+					host:        "#{@config_settings["#{@environment}_host"]}",
+					user:        "#{@config_settings["#{@environment}_user"]}",
+					pass:        "#{@config_settings["#{@environment}_password"]}",
+					environment: "#{@environment}",
+					status:      "\033[32menabled\033[0m"
+				}
+
 				return environment_settings
+
 			when "wp-enabled"
+
 				# load the wp-config file environment settings for wp-enabled environments
-				environment_settings = Hash.new
-				index_map = {
-							'DB_HOST' => :host,
-							'DB_USER' => :user,
-							'DB_PASSWORD' => :pass,
-							'DB_NAME' => :name,
-							'WP_SITEURL' => :site
-							}
-				case @environment
-				when "production"
-					file =	File.read( File.join( Obi::Project.new(@project_name).project_path,
-							"wp-config.php"))[/\}\s*else\s*{(\s*\/[\s|\S]*?|\s*)define\(\s*'DB_NAME[\s|\S]*?}[\s\n\/]/]
-				else
-					file =	File.read( File.join( Obi::Project.new(@project_name).project_path,
-							"wp-config.php"))[/\(\s*WP_ENV\s*==\s*'\s*#{@environment}\s*'\s*\).*{[\s|\S]*?}[\s|\S]*?else/]
-				end
-				file.each_line do |line|
-					key = line[/('|")(.*?)('|")(.*?)('|")(.*?)('|")/,2]
-					value = line[/('|")(.*?)('|")(.*?)('|")(.*?)('|")/,6]
-					environment_settings[index_map[key]] = value
-				end
-				environment_settings[:environment] = "#{@environment}"
-				environment_settings[:status] = "\033[32mwp-enabled\033[0m"
-				return environment_settings.delete_if { |k, v| v.nil? }
-			else
-				# do not allow
+				file = File.read( File.join( Obi::Project.new(@project_name).project_path,
+						"wp-config.php"))[/\$#{@environment}\s*?=[\s|\S]*?({[\s|\S]*?})/, 1]
+
+				wp_config = JSON.parse(file)
+
+				environment_settings  = {
+					host:        "#{wp_config['host']}",
+					user:        "#{wp_config['user']}",
+					pass:        "#{wp_config['pass']}",
+					environment: "#{@environment}",
+					status:      "\033[32mwp-enabled\033[0m"
+				}
+
+				return environment_settings
 			end
 		end
 	end
